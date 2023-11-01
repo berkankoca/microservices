d77b46e2a0a74bfa137ec0b624dad78c5e8ed9fb
berkan <berkan.koca96@gmail.com>
2023-11-01 13:09:32 +0300

    created inventory-service(controller, dto, service, repository, model)

---
+++ inventory-service/src/main/java/com/koca/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,33 @@
+package com.koca.inventoryservice;
+
+import com.koca.inventoryservice.model.Inventory;
+import com.koca.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("redmi_note_10");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_8");
+			inventory1.setQuantity(60);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
+
+}
