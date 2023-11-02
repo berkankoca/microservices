29a28b91fe16d88e390fdf487e2c68d7542245fe
berkan <berkan.koca96@gmail.com>
2023-11-02 09:38:02 +0300

    keycloak

---
+++ discovery-server/src/main/java/com/koca/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.koca.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${eureka.username}")
+    private String EUREKA_USERNAME;
+
+    @Value("${eureka.password}")
+    private String EUREKA_PASSWORD;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(EUREKA_USERNAME).password(EUREKA_PASSWORD)
+                .authorities("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
